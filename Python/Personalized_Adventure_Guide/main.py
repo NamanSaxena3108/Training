d329865aa5a9b55d52c91ea1fcb20550afd46764
NamanSaxena3108 <naman2004saxena@gmail.com>
2024-09-20 12:21:43 +0530

    Updated

--- Python/Personalized_Adventure_Guide/main.py
+++ Python/Personalized_Adventure_Guide/main.py
@@ -1,1 +1,16 @@
-print("Welcome Message")
+#' '
+#" "
+#''' '''
+#""" """ 
+name=input("Hello User: Please Enter your name \n")
+
+print("Welcome to the class 3A "+name)
+
+message='Hello '+name+' Welcome to Personalized Adventure Guide'
+print(message)
+
+message='Hello {var} Welcome to Personalized Adventure Guide'.format(var=name)
+print(message)
+
+print(f"Hello {name.title()}, Welcome to Personalized Adventure Guide")
+
---
+++ Python/Personalized_Adventure_Guide/project1.py
@@ -0,0 +1,2 @@
+name=input("Hello User 😊 Please Enter Your Name \n")
+print(f"Hello {name.title()}, Welcome to Personalized Adventure Guide")
